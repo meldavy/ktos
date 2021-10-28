5361100b336a2a58ff78d341464e263e7adb1648
meldavy <jistyle.jty@gmail.com>
2021-10-27 17:45:48 -0700

    244203_001001.ipf

--- extract/ui.ipf/uiscp/lib_config.lua
+++ extract/ui.ipf/uiscp/lib_config.lua
@@ -4,22 +4,19 @@ function UPDATE_CONTROL_MODE()
 
 	SetLockKeyboardSelectMode(0);
 	local controlmodeType = tonumber(config.GetXMLConfig("ControlMode"));
-	if controlmodeType == 0 then
-		--자동
-		SetChangeUIMode(0)
-	elseif controlmodeType == 1 then
+
+	SetChangeUIMode(controlmodeType);
+
+	if controlmodeType == 1 then
 		--조이패드
-		SetChangeUIMode(1)
 		SetJoystickMode(1)
 		UI_MODE_CHANGE(1)
 	elseif controlmodeType == 2 then
 		--키보드
-		SetChangeUIMode(2)
 		SetJoystickMode(0)
 		UI_MODE_CHANGE(2)
 	elseif controlmodeType == 3 then
 		SetLockKeyboardSelectMode(1);
-		SetChangeUIMode(3);
 		SetJoystickMode(0);
 		UI_MODE_CHANGE(2)
 	end
@@ -33,8 +30,22 @@ function UPDATE_CONTROL_MODE()
 	local modetime = session.GetModeTime();
 	if modetime > 0 then
 		local quickSlotFrame = ui.GetFrame("quickslotnexpbar");
-		QUICKSLOTNEXPBAR_UPDATE_HOTKEYNAME(quickSlotFrame);
-		quickSlotFrame:Invalidate();
+        if quickSlotFrame ~= nil and QUICKSLOTNEXPBAR_UPDATE_HOTKEYNAME ~= nil then
+		    QUICKSLOTNEXPBAR_UPDATE_HOTKEYNAME(quickSlotFrame);
+		    quickSlotFrame:Invalidate();
+        end
+				
+		local restquickslot = ui.GetFrame('restquickslot');
+        if restquickslot ~= nil and RESTQUICKSLOT_UPDATE_HOTKEYNAME ~= nil then
+		    RESTQUICKSLOT_UPDATE_HOTKEYNAME(restquickslot);
+		    restquickslot:Invalidate();
+        end
+
+		local fluting_keyboard = ui.GetFrame('fluting_keyboard');
+        if fluting_keyboard ~= nil and FLUTING_KEYBOARD_UPDATE_HOTKEYNAME ~= nil then
+		    FLUTING_KEYBOARD_UPDATE_HOTKEYNAME(fluting_keyboard);
+		    fluting_keyboard:Invalidate();
+        end
 	end
 end
 
@@ -46,4 +57,22 @@ function UPDATE_OTHER_PC_EFFECT(value)
 	config.EnableOtherPCEffect(tonumber(value));
 end
 
+function UPDATE_NATURAL_EFFECT(value)
+	config.EnableNaturalEffect(tonumber(value));
+end
 
+function UPDATE_OTHER_PC_DAMAGE_EFFECT(value)
+	config.EnableOtherPCDamageEffect(tonumber(value));
+end
+
+function UPDATE_DEAD_PARTS(value)
+	config.EnableDeadParts(tonumber(value));
+end
+
+function UPDATE_SILHOUETTE(value)
+	config.EnableCharSilhouette(tonumber(value));
+end
+
+function UPDATE_GIZMOTARGETAIM(value)
+	config.EnableSkillGizmoTargetAim(tonumber(value));
+end
