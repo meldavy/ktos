87386031e23b759687279594de28cc229cc4b8da
meldavy <jistyle.jty@gmail.com>
2021-10-27 17:50:56 -0700

    274717_001001.ipf

--- extract/addon.ipf/foodtable_ui/foodtable_ui.lua
+++ extract/addon.ipf/foodtable_ui/foodtable_ui.lua
@@ -1,10 +1,8 @@
 
 function FOODTABLE_UI_ON_INIT(addon, frame)
-
 	addon:RegisterMsg("OPEN_FOOD_TABLE_UI", "ON_OPEN_FOOD_TABLE_UI");
-	addon:RegisterMsg("FOOD_ADD_SUCCESS", "ON_FOOD_ADD_SUCCESS");
-	addon:RegisterMsg("FOODTABLE_HISTORY_UI", "ON_FOODTABLE_HISTORY_UI");
-		
+	--addon:RegisterMsg("FOOD_ADD_SUCCESS", "ON_FOOD_ADD_SUCCESS");
+	addon:RegisterMsg("FOODTABLE_HISTORY_UI", "ON_FOODTABLE_HISTORY_UI");		
 end
 
 function ON_FOODTABLE_HISTORY_UI(frame, msg, handle)
@@ -20,7 +18,7 @@ function ON_FOODTABLE_HISTORY_UI(frame, msg, handle)
 	for i = cnt -1 , 0, -1 do
 		local str = session.camp.GetFoodHistoryByIndex(i);
 		if nil ~= str then
-local ctrlSet = log_gbox:CreateControlSet("squire_foodcamp_history", "CTRLSET_" .. i,  ui.CENTER_HORZ, ui.TOP, 55, 0, 0, 0);
+			local ctrlSet = log_gbox:CreateControlSet("squire_foodcamp_history", "CTRLSET_" .. i,  ui.CENTER_HORZ, ui.TOP, 55, 0, 0, 0);
 			local sList = StringSplit(str, "#");
 			local txt = ctrlSet:GetChild("txt");
 			txt:SetTextByKey("text", sList[1]);
@@ -31,75 +29,21 @@ local ctrlSet = log_gbox:CreateControlSet("squire_foodcamp_history", "CTRLSET_" 
 	GBOX_AUTO_ALIGN(log_gbox, 20, 3, 10, true, false);
 end
 
-function SET_FOOD_TABLE_BASE_INFO(ctrlSet, cls, tableInfo)
-		local slot = GET_CHILD(ctrlSet, "slot");
-		local icon = CreateIcon(slot);
-		icon:SetImage(cls.Icon);
-		local itemname = GET_CHILD(ctrlSet, "itemname");
-		itemname:SetTextByKey("value", cls.Name);
-		local itemdesc = GET_CHILD(ctrlSet, "itemdesc");
+function SET_FOOD_TABLE_BASE_INFO(ctrlSet, cls, tableInfo, abilLevel)
+	local slot = GET_CHILD(ctrlSet, "slot");
+	local icon = CreateIcon(slot);
+	icon:SetImage(cls.Icon);
+	local itemname = GET_CHILD(ctrlSet, "itemname");
+	itemname:SetTextByKey("value", cls.Name);
+	local itemdesc = GET_CHILD(ctrlSet, "itemdesc");
 
-		local descFunc = "DESC_FOOD_" .. cls.ClassName;
-		descFunc = _G[descFunc];
-		local descStr = descFunc(tableInfo:GetSkillType(), tableInfo:GetSkillLevel());
-		itemdesc:SetTextByKey("value", descStr);
+	local descFunc = "DESC_FOOD_" .. cls.ClassName;
+	descFunc = _G[descFunc];
+	local descStr = descFunc(tableInfo:GetSkillType(), tableInfo:GetSkillLevel(), abilLevel);
+	itemdesc:SetTextByKey("value", descStr);
 
-		ctrlSet:SetUserValue("FOOD_TYPE", cls.ClassID);
+	ctrlSet:SetUserValue("FOOD_TYPE", cls.ClassID);
 end
-
-function FOODTABLE_CHECK_BOX(parent, ctrl)
-	local tableInfo = session.camp.GetCurrentTableInfo();
-
-	local isMyFoodTable = false;
-	if session.loginInfo.GetAID() == tableInfo:GetAID() then
-		isMyFoodTable = true;
-	end
-
-	if false == isMyFoodTable then
-		return;
-	end
-
-	-- ¿œ¥‹ ∏µÂ ∫Ø∞ÊâÁ¥Ÿ «œ∞Ì,
-	tableInfo:SetSharedFood();
-
-	local titleBox = GET_CHILD(parent, "gBox", "ui::CGroupBox");
-	titleBox:SetVisible(tableInfo:GetSharedFood());
-
-	if 0 == tableInfo:GetSharedFood() then
-		local frame = parent:GetTopParentFrame();
-		local handle = frame:GetUserIValue("HANDLE");	
-		packet.ReqFoodTableTitle(handle, tableInfo:GetSharedFood(), "None");
-	end
-
-end
-
-function CHANGE_FOOD_TABE_TITLE(parent, ctrl)
-	local tableInfo = session.camp.GetCurrentTableInfo();
-
-	local isMyFoodTable = false;
-	if session.loginInfo.GetAID() == tableInfo:GetAID() then
-		isMyFoodTable = true;
-	end
-
-	if false == isMyFoodTable then
-		return;
-	end
-
-	local frame = parent:GetTopParentFrame();
-	local handle = frame:GetUserIValue("HANDLE");	
-	if 0 == tableInfo:GetSharedFood() then
-		packet.ReqFoodTableTitle(handle, tableInfo:GetSharedFood(), "None");
-		return;
-	end
-
-	local edit =  GET_CHILD(parent, "TitleInput")
-	if nil == edit then
-		return;
-	end
-
-	packet.ReqFoodTableTitle(handle, tableInfo:GetSharedFood(), edit:GetText());
-end
-
 
 function ON_OPEN_FOOD_TABLE_UI(frame, msg, handle, forceOpenUI)
 	
@@ -108,7 +52,13 @@ function ON_OPEN_FOOD_TABLE_UI(frame, msg, handle, forceOpenUI)
 	else
 		if frame:IsVisible() == 0 then
 			return;
-		end	
+		end
+		
+		-- UIÍ∞Ä Ïó¥Î†§ÏûàÍ≥† forceOpenUIÍ∞Ä 0Ïù∏ Í≤ΩÏö∞Îäî UI Í∞±Ïã†Ïù∏ Í≤ΩÏö∞Ïù¥ÎØÄÎ°ú, ÎÇ¥Í∞Ä ÌòÑÏû¨ ÏÉÅÌò∏ÏûëÏö© Ï§ëÏù∏ Î∞∞ÏãùÎåÄÏù∏ÏßÄ Ï†ÄÏû•Îêú Ìï∏Îì§Î°ú Ï≤¥ÌÅ¨ÌïúÎã§
+		local tableHandle = frame:GetUserValue("HANDLE");
+		if tableHandle ~= nil and tableHandle ~= handle then
+			return;
+		end
 	end
 
 	frame:SetUserValue("HANDLE", handle);
@@ -126,23 +76,11 @@ function ON_OPEN_FOOD_TABLE_UI(frame, msg, handle, forceOpenUI)
 		isMyFoodTable = true;
 	end
 
-	if true == isMyFoodTable then
-		local ctrlSet = gbox_table:CreateControlSet('food_check_party', "check_Party", 15 , 0);
-		local checkBox = GET_CHILD(ctrlSet, "check_party", "ui::CCheckBox");
-		checkBox:SetCheck(tableInfo:GetSharedFood());
-		local titleBox = GET_CHILD(ctrlSet, "gBox", "ui::CGroupBox");
-		if 0 == tableInfo:GetSharedFood() then
-			titleBox:SetVisible(0);
-		else
-			titleBox:SetVisible(1);
-		end
-	end
-
 	for i = 0 , cnt - 1 do
 		local foodItem = tableInfo:GetFoodItem(i);
 		local ctrlSet = gbox_table:CreateControlSet('camp_food_item', "FOOD" .. i, 0, 0);
 		local cls = GetClassByType("FoodTable", foodItem.type);
-		SET_FOOD_TABLE_BASE_INFO(ctrlSet, cls, tableInfo);
+		SET_FOOD_TABLE_BASE_INFO(ctrlSet, cls, tableInfo, foodItem.abilLevel);
 		local itemcount = GET_CHILD(ctrlSet, "itemcount");
 		itemcount:SetTextByKey("value", foodItem.remainCount);
 
@@ -154,20 +92,24 @@ function ON_OPEN_FOOD_TABLE_UI(frame, msg, handle, forceOpenUI)
 		local gbox_make = gbox:GetChild("gbox_make");
 		gbox_make:RemoveAllChild();
 
+		local myObj = GetMyPCObject();
 		local clslist, cnt  = GetClassList("FoodTable");
 		for i = 0 , cnt - 1 do
 			local cls = GetClassByIndexFromList(clslist, i);
 			
-			local skillInfo = session.GetSkill(tableInfo:GetSkillType());
-			if skillInfo ~= nil then
-				local sklObj = GetIES(skillInfo:GetObject());
-
-				-- ¿ΩΩƒ ¡¶¿€¿« « ø‰ Ω∫≈≥∑π∫ß¿Ã pc¿« Ω∫≈≥ ∑π∫ß ¿Ã«œ¿œ∂ß √‚∑¬
-				if cls.SkillLevel <= sklObj.Level then
-					local ctrlSet = gbox_make:CreateControlSet('camp_food_register', "FOOD_" .. cls.ClassName, 0, 0);
-					SET_FOOD_TABLE_BASE_INFO(ctrlSet, cls, tableInfo);
-					SET_FOOD_TABLE_MATAERIAL_INFO(ctrlSet, cls);		
+			-- ÏùåÏãù Ï†úÏûëÏùò ÌïÑÏöî Ïä§ÌÇ¨Î†àÎ≤®Ïù¥ pcÏùò Ïä§ÌÇ¨ Î†àÎ≤® Ïù¥ÌïòÏùºÎïå Ï∂úÎ†•
+			if cls.SkillLevel <= tableInfo:GetSkillLevel() then
+				local ctrlSet = gbox_make:CreateControlSet('camp_food_register', "FOOD_" .. cls.ClassName, 0, 0);
+				local abilLevel = 0;
+				local abilName = TryGetProp(cls, 'Ability', 'None');
+				if abilName ~= nil and abilName ~= 'None' then
+					local abil = GetAbility(myObj, abilName);
+					if abil ~= nil then
+						abilLevel = TryGetProp(abil, 'Level', 0);
+					end
 				end
+				SET_FOOD_TABLE_BASE_INFO(ctrlSet, cls, tableInfo, abilLevel);
+				SET_FOOD_TABLE_MATAERIAL_INFO(ctrlSet, cls);
 			end
 		end
 
@@ -182,10 +124,8 @@ function ON_OPEN_FOOD_TABLE_UI(frame, msg, handle, forceOpenUI)
 		local index = tab:GetIndexByName("tab_normal")
 		tab:SetTabVisible(2, false);
 		tab:SelectTab(index);
-		tab:ShowWindow(1);
-		
+		tab:ShowWindow(1);		
 	end
-	
 end
 
 function SET_FOOD_TABLE_MATAERIAL_INFO(ctrlSet, cls)
@@ -253,7 +193,7 @@ function EXEC_MAKE_FOODTABLE_ITEM(foodType, cnt, handle)
 
 end
 
-function ON_FOOD_ADD_SUCCESS(frame)
+function ON_FOOD_ADD_SUCCESS()
 	local frame = ui.GetFrame("foodtable_ui");
 	local gbox = frame:GetChild("gbox");
 	local itembox = GET_CHILD(gbox, "itembox");
@@ -266,27 +206,46 @@ function REMOVE_FOOD_TABLE(parent, ctrl)
 
 	local frame = parent:GetTopParentFrame();
 	local handle = frame:GetUserIValue("HANDLE");
-	control.CustomCommand("REMOVE_FOODTABLE", handle);	
+	control.CustomCommand("REMOVE_FOODTABLE", handle);
+	ui.CloseFrame(frame:GetName());
 
 end
 
-function DESC_FOOD_salad(skillType, skillLevel)
+function DESC_FOOD_salad(skillType, skillLevel, abilLevel)
     local value = 7.5 + skillLevel * 2.5;
-	return ScpArgMsg("IncreaseHP{Value}For{Time}Minute", "Value", value, "Time", 30);
+	return ScpArgMsg("IncreaseHP{Value}For{Time}Minute", "Value", value, "Time", 30 + abilLevel);
 end
 
-function DESC_FOOD_sandwich(skillType, skillLevel)
+function DESC_FOOD_sandwich(skillType, skillLevel, abilLevel)
     local value = 7.5 + skillLevel * 2.5;
-	return ScpArgMsg("IncreaseSP{Value}For{Time}Minute", "Value", value, "Time", 30);
+	return ScpArgMsg("IncreaseSP{Value}For{Time}Minute", "Value", value, "Time", 30 + abilLevel);
 end
 
-function DESC_FOOD_soup(skillType, skillLevel)
+function DESC_FOOD_soup(skillType, skillLevel, abilLevel)
     local value = skillLevel;
-	return ScpArgMsg("IncreaseRHPTIME{Value}For{Time}Minute", "Value", value, "Time", 30);
+	return ScpArgMsg("IncreaseRHPTIME{Value}For{Time}Minute", "Value", value, "Time", 30 + abilLevel);
 end
 
-function DESC_FOOD_yogurt(skillType, skillLevel)
+function DESC_FOOD_yogurt(skillType, skillLevel, abilLevel)
     local value = skillLevel;
-	return ScpArgMsg("IncreaseRSPTIME{Value}For{Time}Minute", "Value", value, "Time", 30);
+	return ScpArgMsg("IncreaseRSPTIME{Value}For{Time}Minute", "Value", value, "Time", 30 + abilLevel);
 end
 
+function DESC_FOOD_BBQ(skillType, skillLevel, abilLevel)
+    local value = 0.5 + (skillLevel - 5) * 0.5;
+    value = math.floor(value)
+	return ScpArgMsg("IncreaseSR{Value}For{Time}Minute", "Value", value, "Time", 30 + abilLevel);
+end
+
+function DESC_FOOD_champagne(skillType, skillLevel, abilLevel)
+    local value = 0.5 + (skillLevel - 5) * 0.5;
+    value = math.floor(value)
+	return ScpArgMsg("IncreaseSDR{Value}For{Time}Minute", "Value", value, "Time", 30 + abilLevel);
+end
+
+function FOODTABLE_UI_CLOSE_FRAME(handle)
+    local frame = ui.GetFrame('foodtable_ui');
+    if handle == frame:GetUserIValue('HANDLE') then
+        ui.CloseFrame('foodtable_ui');
+    end
+end
