4b1fece5da3f991a368e0fc162d82b74e53c8d0c
meldavy <jistyle.jty@gmail.com>
2021-10-27 17:40:24 -0700

    213575_001001.ipf

--- extract/ui.ipf/uiscp/buff_tooltip.lua
+++ extract/ui.ipf/uiscp/buff_tooltip.lua
@@ -4,23 +4,87 @@ function UPDATE_PREMIUM_TOOLTIP(tooltipframe, strarg, numarg1, numarg2)
 	local typeStr = "None";
 	local token_expup = tooltipframe:GetChild("token_expup");
 	local token_staup = tooltipframe:GetChild("token_staup");
+	local marketFastGet = tooltipframe:GetChild("marketFastGet");
+	local marketMinMax = tooltipframe:GetChild("marketMinMax");
+--	local token_buffCountUp = tooltipframe:GetChild("token_buffCountUp");
+	local token_teamwarehouse = tooltipframe:GetChild("token_teamwarehouse");
+--	local mission_reward = tooltipframe:GetChild("mission_reward");
+--	local RaidStance = tooltipframe:GetChild("RaidStance");
+    
+	local token_tradecount = tooltipframe:GetChild("token_tradecount");
+	
+	local pcbangPartyExpUp = tooltipframe:GetChild("pcbangPartyExpUp");
+	pcbangPartyExpUp:ShowWindow(0);
+	
+	local pcbangChallengeCount = tooltipframe:GetChild("challengeCount");
+	pcbangChallengeCount:ShowWindow(0);
+	
+	local pcbangItemRental = tooltipframe:GetChild("pcbangItemRental");
+	pcbangItemRental:ShowWindow(0);
+	
+	
 
-	if ITEM_TOKEN == numarg1 then
+	local buffCls = GetClassByType('Buff', numarg1);
+	local argNum = NONE_PREMIUM;
+	if buffCls.ClassName == "Premium_Nexon" then
+		argNum = NEXON_PC;
+	elseif buffCls.ClassName =="Premium_Token" then
+		argNum = ITEM_TOKEN;
+	end
+
+	if ITEM_TOKEN == argNum then
 		type:SetTextByKey("value", ClMsg("tokenItem"));
+		
+        marketFastGet:ShowWindow(1);
+        marketMinMax:ShowWindow(1);
+        token_teamwarehouse:ShowWindow(1);
+--        token_buffCountUp:ShowWindow(1);
 		token_expup:SetTextByKey("value", ScpArgMsg("Token_ExpUp{PER}", "PER", "20%"));
 		token_staup:SetTextByKey("value", ClMsg("AllowPremiumPose"));
-	elseif NEXON_PC == numarg1 then
+		local accountObj = GetMyAccountObj();
+		if accountObj.TradeCount > 0 then
+			local tradeCountString = ScpArgMsg("AllowTradeByCount") .. " " .. tostring(accountObj.TradeCount)
+			token_tradecount:SetTextByKey("value", tradeCountString);
+			token_tradecount:ShowWindow(1);
+		else
+			token_tradecount:ShowWindow(0);
+		end
+--        mission_reward:ShowWindow(1);
+--        RaidStance:ShowWindow(1);
+
+	elseif NEXON_PC == argNum then
 		type:SetTextByKey("value", ClMsg("nexon")); 
-		token_staup:SetTextByKey("value", ClMsg("token_setup"));
+--		token_staup:SetTextByKey("value", ClMsg("token_setup"));
 		token_expup:SetTextByKey("value", ClMsg("token_expup"));
+		token_tradecount:ShowWindow(0);
+--		token_buffCountUp:ShowWindow(0);
+        marketFastGet:ShowWindow(0);
+        marketMinMax:ShowWindow(0);
+        token_teamwarehouse:ShowWindow(0);
+--        mission_reward:ShowWindow(0);
+--        RaidStance:ShowWindow(0);
+		
+		local pcbangChallengeCountString = ScpArgMsg("PcbangChallengeCount{COUNT}", "COUNT", 1)
+		pcbangChallengeCount:SetTextByKey("value", pcbangChallengeCountString);
+		pcbangChallengeCount:ShowWindow(1);
+		
+		pcbangPartyExpUp:SetTextByKey("value", ScpArgMsg("PcbangPartyExpUp"));
+		pcbangPartyExpUp:ShowWindow(1);
+		
+		pcbangItemRental:SetTextByKey("value", ScpArgMsg("PcbangItemRental"));
+		pcbangItemRental:ShowWindow(1);
+		
+	else
+		token_tradecount:ShowWindow(0);
+        marketFastGet:ShowWindow(0);
+        marketMinMax:ShowWindow(0);
 	end
 	
 	for i = 0, 3 do 
-		local str = GetCashTypeStr(numarg1, i)
+		local str, value = GetCashInfo(argNum, i)
 		if nil ~= str then
 			type = tooltipframe:GetChild(str);
 			local normal = GetCashValue(0, str);
-			local value = GetCashValue(numarg1,str);
 			local txt = "None"
 			if str == "marketSellCom" then
 				normal = normal + 0.01;
@@ -28,6 +92,9 @@ function UPDATE_PREMIUM_TOOLTIP(tooltipframe, strarg, numarg1, numarg2)
 				txt = math.floor(normal*100).. "% ->".. math.floor(value*100) .."%";
 				type:SetTextByKey("value", ClMsg(str)); 
 			elseif str =="abilityMax" or str == "speedUp"then
+				if NEXON_PC == argNum and str =="abilityMax" then
+					str = "abilityMax_ForPC";
+				end
 				txt = normal.. " -> +"..value;
 				type:SetTextByKey("value", ScpArgMsg(str.."{COUNT}", "COUNT", value)); 
 			else
@@ -36,12 +103,31 @@ function UPDATE_PREMIUM_TOOLTIP(tooltipframe, strarg, numarg1, numarg2)
 			end		
 		end
 	end
+
+	local cnt = tooltipframe:GetChildCount();
+	local y = 45;
+	for i = 0, cnt - 1 do
+		local ctrl = tooltipframe:GetChildByIndex(i);
+		if ctrl:IsVisible() == 1 and ctrl:GetClassString() == "ui::CRichText" and ctrl:GetName() ~= "richtext_1" then
+			ctrl:SetOffset(ctrl:GetX(), y);
+			y = y + ctrl:GetHeight();
+		end
+	end
+
+	local gbox = tooltipframe:GetChild("gbox");
+	gbox:Resize(gbox:GetWidth(), y + 10);
+	tooltipframe:Resize(tooltipframe:GetWidth(), gbox:GetHeight() + 20);
 end
 
 
 function UPDATE_BUFF_TOOLTIP(frame, handle, numarg1, numarg2)
+	local buff = nil;    
+    if tonumber(numarg2) > 0 then
+        buff = info.GetBuff(handle, numarg1, numarg2);
+    else
+        buff = info.GetBuff(handle, numarg1);
+    end
 
-	local buff 					= info.GetBuff(handle, numarg1);
 	local buffOver;
 	local buffTime;
 	if buff ~= nil then
@@ -59,8 +145,6 @@ function UPDATE_BUFF_TOOLTIP(frame, handle, numarg1, numarg2)
 		nametxt = nametxt .. " X " .. buffOver;
 	end
 
-	name:SetText("{@st41}"..nametxt);
-
 	local comment = frame:GetChild("comment");
 
 	local tooltipfunc = _G["BUFF_TOOLTIP_" .. cls.ClassName];
@@ -68,7 +152,11 @@ function UPDATE_BUFF_TOOLTIP(frame, handle, numarg1, numarg2)
 	if tooltipfunc == nil then
 		tooltip = cls.ToolTip;
 	else
-		tooltip = tooltipfunc(buff, cls);
+		local newName;
+		tooltip, newName = tooltipfunc(buff, cls);
+		if newName ~= nil then
+			nametxt = newName;
+		end
 	end
 
 	if buffTime == 0.0 then
@@ -82,19 +170,68 @@ function UPDATE_BUFF_TOOLTIP(frame, handle, numarg1, numarg2)
 		comment:SetText("{@st59}"..txt);
 	end
 
+	name:SetText("{@st41}"..nametxt);
+
 end
 
 function BUFF_TOOLTIP_Rejuvenation(buff, cls)
 
 	local heal = 5
-	return string.format(ScpArgMsg("Auto_HPwa_SPLeul_HoeBogSiKipNiDa."), heal);
+	return string.format(ScpArgMsg("Auto_HPwa_SPLeul_HoeBogSiKipNiDa."), heal), nil;
 
 end
 
 function BUFF_TOOLTIP_OgouVeve(buff, cls)
 
-	return ScpArgMsg("Auto_HimKwa_JiNeungeul_olLyeoJupNiDa.");
+	return ScpArgMsg("Auto_HimKwa_JiNeungeul_olLyeoJupNiDa."), nil;
 
 end
 
 
+function BUFF_TOOLTIP_TeamLevel(buff, cls)
+
+	local advantageText = "";
+	local expBonus = GET_TEAM_LEVEL_EXP_BONUS(buff.arg1);
+	advantageText = advantageText .. ScpArgMsg("ExpGetAmount") .. " + " .. expBonus .. "%";
+
+	return advantageText, ScpArgMsg("TeamLevel") .. " " .. buff.arg1;
+
+end
+
+
+function BUFF_TOOLTIP_DRUG_LOOTINGCHANCE(buff, cls)
+
+	local advantageText = "";
+	local buffArg1 = buff.arg1;
+	advantageText = advantageText .. ScpArgMsg("DrugLootingChance") .. " + " .. buffArg1;
+
+	return advantageText, ScpArgMsg("DrugLootingChance") .. " " .. buff.arg1;
+
+end
+
+function GET_TEAM_LEVEL_EXP_BONUS(teamLevel)
+    local expBonus = 0;
+    local xpCls = GetClassByType("XP_TeamLevel", teamLevel);
+	local xpAmount = TryGetProp(xpCls, 'ExpBonus');
+    if xpAmount ~= nil and xpAmount > 0 then
+        expBonus = xpAmount;
+    end
+    return expBonus;
+end
+
+function BUFF_TOOLTIP_Event_CharExpRate(buff, cls)
+	local advantageText = "";
+	if buff ~= nil then
+		advantageText = advantageText .. ScpArgMsg("ExpGetAmount") .. " + " .. math.floor(buff.arg1) .. "%";	
+	end
+	return advantageText, cls.ToolTip;
+end
+
+
+function BUFF_TOOLTIP_Achieve_Possession_Buff(buff, cls)
+
+	local advantageText = "";
+	local grade_Num = buff.arg1
+	advantageText = ScpArgMsg("ACHIEVE_GRADE_EXP"..grade_Num);
+	return advantageText, ScpArgMsg("ACHIEVE_GRADE", "num", grade_Num);
+end
--- extract/ui.ipf/uiscp/lib_uiscp.lua
+++ extract/ui.ipf/uiscp/lib_uiscp.lua
@@ -21,14 +21,38 @@ function INIT_MAP_PICTURE_UI(pic, mapName, hitTest)
 
 end
 
-function DISABLE_BUTTON_DOUBLECLICK(framename,buttonname)
+function DISABLE_BUTTON_DOUBLECLICK_WITH_CHILD(framename,childname,buttonname)
 
+	local frame = ui.GetFrame(framename)
+	local child = GET_CHILD_RECURSIVELY(frame,childname)
+	local btn = GET_CHILD_RECURSIVELY(child,buttonname)
+
+	local strScp = string.format("ENABLE_BUTTON_DOUBLECLICK_WITH_CHILD(\"%s\",\"%s\", \"%s\")", framename, childname, buttonname);
+
+	ReserveScript(strScp, 5);
+	btn:SetEnable(0)
+end
+
+function ENABLE_BUTTON_DOUBLECLICK_WITH_CHILD(framename,childname,buttonname)
+
+	local frame = ui.GetFrame(framename)
+	local child = GET_CHILD_RECURSIVELY(frame,childname)
+	local btn = GET_CHILD_RECURSIVELY(child,buttonname)
+	btn:SetEnable(1)
+
+end
+
+function DISABLE_BUTTON_DOUBLECLICK(framename,buttonname, sec)
+	local delay = 5;
+	if sec ~= nil then
+		delay = sec;
+	end
 	local frame = ui.GetFrame(framename)
 	local btn = GET_CHILD_RECURSIVELY(frame,buttonname)
 
 	local strScp = string.format("ENABLE_BUTTON_DOUBLECLICK(\"%s\", \"%s\")", framename, buttonname);
 
-	ReserveScript(strScp, 5);
+	ReserveScript(strScp, delay);
 	btn:SetEnable(0)
 end
 
@@ -179,14 +203,15 @@ function MAP_UPDATE_PARTY(frame, msg, arg, type, info)
 end
 
 function MAP_UPDATE_GUILD(frame, msg, arg, type, info)
-
-	DESTROY_CHILD_BYNAME(frame, 'GM_');
+	DESTROY_CHILD_BYNAME(frame, 'GM_');        
+    if session.world.IsIntegrateServer() == true then        
+        DESTROY_GUILD_MEMBER_ICON()        
+        return
+    end
 
 	local mapprop = session.GetCurrentMapProp();
 	local list = session.party.GetPartyMemberList(PARTY_GUILD);
 	local count = list:Count();
-	
-
 	if count == 1 then
 		return;
 	end
@@ -286,7 +311,7 @@ function SET_MAP_MONGEN_NPC_INFO(picture, mapprop, WorldPos, MonProp, mapNpcStat
 
 	local cheat = string.format("//setpos %d %d %d", WorldPos.x, WorldPos.y, WorldPos.z);
 	local scpstr = string.format( "ui.Chat(\"%s\")", cheat);
-	picture:SetEventScript(ui.LBUTTONUP, scpstr);
+	picture:SetEventScript(ui.LBUTTONUP, scpstr, true);
 
 	local idx = GET_NPC_STATE(MonProp:GetDialog(), statelist, npclist, questIESlist);
 	local Icon, state, questclsid, iconState = GET_NPC_ICON(idx, statelist, questIESlist);
@@ -321,7 +346,7 @@ function SET_MAP_MONGEN_NPC_INFO(picture, mapprop, WorldPos, MonProp, mapNpcStat
 	local questclsIdStr = '';
 	local cnt = #npclist;
 	for i = 1 , cnt do
-		local name = npclist[i];
+		local name = npclist[i];        
 		if  MonProp:IsHaveDialog(name) then
 			local questIES = questIESlist[i];
 			local result = SCR_QUEST_CHECK_C(pc, questIES.ClassName);
@@ -434,58 +459,129 @@ function BUFF_TIME_UPDATE(handle, buff_ui)
 	if updated == 1 then
 		ui.UpdateVisibleToolTips("buff");
 	end
-
-
 end
 
-
-function GET_QUEST_NPC_NAMES(mapname, npclist, statelist, questIESList, questPropList)
-
+function CHECK_QUEST_NPC_NAME(mapname, index)
 	local idx = 1;
 	local pc = GetMyPCObject();
 	local questIES = nil;
 	local cnt = GetClassCount('QuestProgressCheck')
-	for i = 0, cnt - 1 do
-		questIES = GetClassByIndex('QuestProgressCheck', i);
-		if questIES.ClassName ~= 'None' then
-    		local result = SCR_QUEST_CHECK_C(pc,questIES.ClassName);
+	local subQuestZoneList = {}
 
-    		if result ~= 'IMPOSSIBLE' then
-    		    local flag = 0
-    		    
-    		    if questIES.PossibleUI_Notify == 'UNCOND' or result ~= 'POSSIBLE' then
-    		        flag = 1
-    		    end
-    		    
-    		    if flag == 0 then
-    		        if questIES.QuestStartMode == 'NPCENTER_HIDE' 
-    		        or questIES.QuestStartMode == 'GETITEM' 
-    		        or questIES.QuestStartMode == 'USEITEM'
-    		        or questIES.PossibleUI_Notify == 'NO' then
-    				else
-    				    flag = 1
-    				end
-    		    end
-    		    
-    		    if result == "POSSIBLE" and SCR_POSSIBLE_UI_OPEN_CHECK(pc, questIES) == "HIDE" then
-    		        flag = 0
-    		    end
-    		    
-    		    if flag == 1 then
-    		        local State = CONVERT_STATE(result);
-        			local questMap = questIES[State .. 'Map'];
-					local npcname = questIES[State .. 'NPC'];
-                    
-					--if npcname ~= 'None' then
-						npclist[idx] = npcname;
-						statelist[idx] = result;
-						questIESList[idx] = questIES;
-						questPropList[idx] = geQuestTable.GetPropByIndex(i);
-						idx = idx + 1;
-					--end
-    		    end
+	questIES = GetClassByIndex('QuestProgressCheck', index);
+	if questIES.ClassName ~= 'None' then
+    	local result = SCR_QUEST_CHECK_C(pc, questIES.ClassName);
+    	if result ~= 'IMPOSSIBLE' and result ~= "None" then
+    		local flag = 0;
+
+    		if questIES.PossibleUI_Notify == 'UNCOND' or result ~= 'POSSIBLE' then
+    		    flag = 1
     		end
+
+    		if flag == 0 then
+    		    if questIES.QuestStartMode == 'NPCENTER_HIDE' 
+    		    or questIES.QuestStartMode == 'GETITEM' 
+    		    or questIES.QuestStartMode == 'USEITEM'
+    		    or questIES.PossibleUI_Notify == 'NO' then
+    			else
+    				flag = 1
+    			end
+    		end
+
+    		local result2
+    		result2, subQuestZoneList = SCR_POSSIBLE_UI_OPEN_CHECK(pc, questIES, subQuestZoneList, 'ZoneMap')
+    		if result == "POSSIBLE" and result2 == "HIDE" then
+    		    flag = 0
+    		end
+
+    		if flag == 1 then
+    		    local State = CONVERT_STATE(result);
+        		local questMap = questIES[State .. 'Map'];
+				local npcname = questIES[State .. 'NPC'];
+
+				return result, npcname, "YES";
+    		end
+    	end
+	end
+
+	return "None", "None", "NO";
+end
+
+function GET_QUEST_NPC_NAMES(mapname)
+	RequestUpdateMinimap(mapname, 0);
+	local npclist, statelist, questIESList, questPropList = GetQuestNpcNames(mapname);
+	
+	return npclist, statelist, questIESList, questPropList;
+end
+
+function GET_JOB_ICON(job)
+
+	local cls = GetClassByType("Job", job);
+	if cls == nil then
+		return "None";
+	end
+
+	return cls.Icon;
+
+end
+
+
+function GET_MON_ILLUST(monCls)
+
+	if monCls == nil then
+		return "unknown_monster";
+	end
+
+	local name = monCls.Journal;
+	if ui.IsImageExist(name) == true then
+		return name;
+	end
+	
+	name = "mon_"..name
+	if ui.IsImageExist(name) == true then
+		return name;
+	end
+
+	name = monCls.Icon;
+	if ui.IsImageExist(name) == true then
+		return name;
+	end
+	
+	return "unknown_monster";
+end
+
+function SET_SPINE_TOOLTIP_IMAGE(picture, itemCls)
+	local isEnableSpine = config.GetXMLConfig("EnableAnimateItemIllustration");
+
+	local isSpinePicture = false;
+	if isEnableSpine == 1 then
+		local spineToolTip = TryGetProp(itemCls, "SpineTooltipImage");
+		if spineToolTip ~= nil and spineToolTip ~= "None" then
+			local spineInfo = geSpine.GetSpineInfo(spineToolTip);
+			if spineInfo ~= nil then
+				picture:SetScaleFactor(spineInfo:GetScaleFactor());
+				picture:SetOffsetX(spineInfo:GetOffsetX());
+				picture:SetOffsetY(spineInfo:GetOffsetY());
+				picture:CreateSpineActor(spineInfo:GetRoot(), spineInfo:GetAtlas(), spineInfo:GetJson(), "", spineInfo:GetAnimation());
+
+				local effectCount = spineInfo:GetEffectCount();
+				for i = 0, effectCount - 1 do
+					local effect = spineInfo:GetEffect(i);
+					if effect ~= nil then
+						local isOverlab = false;
+						if effect:GetOverlab() == "Yes" then
+							isOverlab = true;
+						end
+						picture:PlayEffectByBone(effect:GetCondition(), effect:GetAttachBoneName(), effect:GetEffectName(), effect:GetScale(), effect:GetSound(), effect:GetPlayTime(), effect:GetDuration(), effect:GetOffsetX(), effect:GetOffsetY(), effect:GetEvent(), "", isOverlab);
+					end
+				end
+
+				isSpinePicture = true;
+			end
 		end
 	end
 
+	if isSpinePicture == false then
+		picture:SetImage(TryGetProp(itemCls, "TooltipImage"));
+	end
 end
